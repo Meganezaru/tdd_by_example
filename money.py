9911833286acb8b72afad038cca74228368c214a
Kazuya Goto <kazuya.goto@imao.biz>
2019-02-15 11:47:04 +0900

    FrancクラスをMoneyクラスから拡張[green:リファクタリング]

--- money.py
+++ money.py
@@ -11,12 +11,6 @@ class Dollar(Money):
         return Dollar(self._amount * multiplier)
 
 
-class Franc:
-    def __init__(self, amount):
-        self._amount = amount
-
+class Franc(Money):
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
-
-    def __eq__(self, other):
-        return self._amount == other._amount
