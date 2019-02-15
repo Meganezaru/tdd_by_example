fbd10f6412d9a5618155e8dc83ae2751501483f7
Kazuya Goto <kazuya.goto@imao.biz>
2019-02-15 11:41:39 +0900

    __eq__()をMoneyクラスへ移動[green:リファクタリング]

--- money.py
+++ money.py
@@ -2,13 +2,13 @@ class Money:
     def __init__(self, amount):
         self._amount = amount
 
+    def __eq__(self, other):
+        return self._amount == other._amount
+
 
 class Dollar(Money):
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
-
-    def __eq__(self, other):
-        return self._amount == other._amount
 
 
 class Franc:
