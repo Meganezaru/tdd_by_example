2803d7c72f174b8843223cfc9c831d694060c802
Kazuya Goto <kazuya.goto@imao.biz>
2019-02-15 15:45:57 +0900

    通貨を戻すメソッドを定義[red:running]

--- money.py
+++ money.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+
+
 class Money:
     def __init__(self, amount):
         self._amount = amount
@@ -13,12 +16,26 @@ class Money:
     def franc(cls, amount):
         return Franc(amount)
 
+    @abstractmethod
+    def times(self, multiplier):
+        pass
+
+    @abstractmethod
+    def currency(self):
+        pass
+
 
 class Dollar(Money):
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
 
+    def currency(self):
+        pass
+
 
 class Franc(Money):
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
+
+    def currency(self):
+        pass
